75cc561f08b051aa89b799769ec6aae4298dbfcf
CarloLim <CarloLim01>
2021-12-10 21:30:39 +0800

    Program 2 (Importing and Adding time interval)

--- Program2.py
+++ Program2.py
@@ -5,30 +5,43 @@
 # Repeat asking the user until the random number has been guessed correctly.
 
 import random
+import time
 
 print("GUESS THE NUMBER")
-print("ELCOME TO GUESS A NUMBER GAME!")
+time.sleep(1)
+print("WELCOME TO GUESS A NUMBER GAME!")
 
+time.sleep(2)
 name = input("Enter your name: ")
 print(f"HI {name}, GUESS A NUMBER BETWEEN 0 - 100 TO WIN!")
+time.sleep(2)
 print(f"{name}, GET READY TO GUESS!")
 
+time.sleep(3)
 user_number = int(input("Your Guess Number: "))
 random_number = random.randint(0,100)
 
 while random_number != user_number:
     if user_number < random_number:
+        time.sleep(1)
         print("Guess Greater Than Your Input Number.")
+        time.sleep(1)
         user_number = int(input("Enter Again Your Guess Number: "))
         if user_number == random_number:
+            time.sleep(2)
             print(f"RANDOM NUMBER: {random_number}")
+            time.sleep(2)
             print(f"CONGRATULATIONS {name}! YOU GUESS THE CORRECT NUMBER!")
             break
     elif user_number > random_number:
+        time.sleep(1)
         print("Guess Less Than Your Input Number.")
+        time.sleep(1)
         user_number = int(input("Enter Again Your Guess Number: "))
+        time.sleep(2)
         if user_number == random_number:
             print(f"RANDOM NUMBER: {random_number}")
+            time.sleep(2)
             print(f"CONGRATULATIONS {name}! YOU GUESS THE CORRECT NUMBER!")
             break
 
