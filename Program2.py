0bcf707c5b9adf2c3c2b9b8fa39fb23b2d49f921
CarloLim <CarloLim01>
2021-12-10 21:26:48 +0800

    Program 2 (Adding intro and outro)

--- Program2.py
+++ Program2.py
@@ -6,21 +6,30 @@
 
 import random
 
+print("GUESS THE NUMBER")
+print("ELCOME TO GUESS A NUMBER GAME!")
+
+name = input("Enter your name: ")
+print(f"HI {name}, GUESS A NUMBER BETWEEN 0 - 100 TO WIN!")
+print(f"{name}, GET READY TO GUESS!")
+
 user_number = int(input("Your Guess Number: "))
 random_number = random.randint(0,100)
 
 while random_number != user_number:
-    if user_number < random_number: 
-        print("Guess Greater Than Your Input Number.") 
+    if user_number < random_number:
+        print("Guess Greater Than Your Input Number.")
         user_number = int(input("Enter Again Your Guess Number: "))
         if user_number == random_number:
             print(f"RANDOM NUMBER: {random_number}")
-            print(f"CONGRATULATIONS! YOU GUESS THE CORRECT NUMBER!")
+            print(f"CONGRATULATIONS {name}! YOU GUESS THE CORRECT NUMBER!")
             break
-    elif user_number > random_number:    
-        print("Guess Less Than Your Input Number.")   
+    elif user_number > random_number:
+        print("Guess Less Than Your Input Number.")
         user_number = int(input("Enter Again Your Guess Number: "))
         if user_number == random_number:
             print(f"RANDOM NUMBER: {random_number}")
-            print(f"CONGRATULATIONS! YOU GUESS THE CORRECT NUMBER!")
+            print(f"CONGRATULATIONS {name}! YOU GUESS THE CORRECT NUMBER!")
             break
+
+print("-THANK YOU FOR PLAYING GUESS THE NUMBER-")
