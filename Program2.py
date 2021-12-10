428e89eda393056bbd7d21059f67758e0040f073
CarloLim <CarloLim01>
2021-12-10 21:15:32 +0800

    Program 2 (Adding while, if, elif and random number)

--- Program2.py
+++ Program2.py
@@ -4,4 +4,13 @@
 # Display “Less than” if the inputed number is less than the random number
 # Repeat asking the user until the random number has been guessed correctly.
 
+import random
+
 user_number = int(input("Your Guess Number: "))
+random_number = random.randint(0,100)
+
+while random_number != user_number:
+    if user_number < random_number:
+        print("Guess Greater Than Your Input Number.")
+    elif user_number > random_number:
+        print("Guess Less Than Your Input Number.")
