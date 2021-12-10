94e13a2b5b64d8a5bc261580d1f7692e3380421c
CarloLim <CarloLim01>
2021-12-10 20:35:53 +0800

    Program 1 (Asking three numbers to the user)

---
+++ Program1.py
@@ -0,0 +1,11 @@
+#Create a program that ask 3 numbers (0-9) from the user.
+#Generate 3 random winning numbers (0-9)
+#Display “Winner” if all 3 input numbers matched the generated numbers
+#Display ”You loss” if not
+#Display ”Try again y/n” after each game
+#If the user enter “y” the user will lottery again
+#if “n” the program will exit.
+
+first = int(input("Enter your First number: "))
+second = int(input("Enter your Second number: "))
+third = int(input("Enter your Third number: "))
